88938c0deeab3ad578d3a86dc0a1b349831c6daf
ReinaOoba <diary.massage.love@gmail.com>
2022-07-25 14:33:50 +0000

    [ADD]bread crumb system

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,7 +1,63 @@
-crumb :root do
-  link "Home", root_path
+crumb :admin_root do
+  link "管理者トップページ", admin_root_path
 end
 
+crumb :admin_search do
+  link "検索結果", admin_search_path
+  parent :admin_root
+end
+
+crumb :admin_article_index do
+  link "記事一覧", admin_articles_path
+  parent :admin_root
+end
+
+crumb :admin_article_show do
+  link "記事詳細", admin_article_path(@admin)
+  parent :admin_article_index
+end
+
+crumb :admin_user_index do
+  link "会員一覧", admin_users_path
+  parent :admin_root
+end
+
+crumb :admin_user_show do
+  link "会員詳細", admin_user_path(@user)
+  parent :admin_users_path
+end
+
+crumb :admin_category_index do
+  link "カテゴリー一覧", admin_categories_path
+  parent :admin_root
+end
+
+crumb :admin_category_show do
+  link "カテゴリー詳細", admin_category_path(@category)
+  parent :admin_category_index
+end
+
+crumb :admin_tag_index do
+  link "タグ一覧", admin_tags_path
+  parent :admin_root
+end
+
+crumb :admin_tag_show do
+  link "タグ詳細", admin_tag_path(@tag)
+  parent :admin_tag_index
+end
+
+crumb :root do
+  link "トップページ", root_path
+end
+
+
+
+
+# crumb : do
+#   link "",
+#   parent :
+# end
 # crumb :projects do
 #   link "Projects", projects_path
 # end
